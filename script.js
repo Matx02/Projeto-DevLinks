11b3321be29a836747f926dec7e1f4183d13ffb6
Matheus <matheussa.com.br@gmail.com>
2024-12-01 11:07:44 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")  
+  
+  //pegar a tag img
+  //const Img = document.querySelector('#profile img')
+  
+  //Substituir a imagem
+  //if (html.classList.contains('light')) {	
+    //Se tiver light mode, adicionar a imagem light
+    //Img.setAttribute("src", "./assets/avatar-light.png")
+ 
+  //} else {
+    //Se tiver sem light mode, manter a imagem normal
+    //Img.setAttribute("src", "./assets/avatar.png")
+  //}
+  
+
+
+
+
+
+}
